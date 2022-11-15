4c3b8e1fe3c8c42481643663c4e357b66c4e743c
chinnadurai <chinna619007@gmail.com>
2022-11-15 06:27:25 +0530

    Updates

---
+++ precache-manifest.451cfcf35b28eec16a9ecc60a1a6da23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ea47c6f24cbda657b640fdee66019a1",
+    "url": "/React-App/index.html"
+  },
+  {
+    "revision": "0208db330b19adcf6a98",
+    "url": "/React-App/static/css/main.39499631.chunk.css"
+  },
+  {
+    "revision": "02f370d4b8bc4f7c37b0",
+    "url": "/React-App/static/js/2.e933fbc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App/static/js/2.e933fbc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0208db330b19adcf6a98",
+    "url": "/React-App/static/js/main.f763bcdd.chunk.js"
+  },
+  {
+    "revision": "9fba2775d24a6f1b31fb",
+    "url": "/React-App/static/js/runtime-main.05c519b6.js"
+  }
+]);
